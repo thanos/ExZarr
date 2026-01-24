14c74bb2df44fc68b8884ee3325eeab9dc2145a8
thanos <thanosv@gmail.com>
2026-01-24 11:54:40 -0500

    closed #87 - Integration tests fail

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,1 +1,5 @@
+# Exclude tests that require external services by default
+# To run these tests, use: mix test --include s3 --include gcs --include azure --include mongo
+ExUnit.configure(exclude: [:s3, :gcs, :azure, :mongo])
+
 ExUnit.start()
